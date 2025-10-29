27e296edc4b84729bc293abc4b427299b9fc3590
bestatclash <743376@student.dusd.net>
2025-10-29 13:17:04 -0700

    Variables And Functions
    i completed all the scripts

---
+++ P6CobianScriptingProject/Assets/BehaviorComponint.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehavior : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     if input . GetKeyDown(KeyCode.R))
+    }
+}GetComponint<Renderer>().material.color = Color.red;
+}
+if (Input.GetKeyDown(KeyCode.B)
+{
+    GetComponent<Renderer>().material.color = Color.blue;
+         }
+     }
+ }
---
+++ P6CobianScriptingProject/Assets/Scripts/VariableAndFunction.cs
@@ -0,0 +1,33 @@
+
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class VariableAndFunction : MonoBehaviour
+{
+    //create an innterger variable
+    int myInt = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //run the variiable through my function  
+        myInt = MultiplyByTwo(myInt);
+        //display the result in the console
+        Debug.Log(myInt);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {//creating my function
+        int MultiplyByTwo(int number)
+       //creat a results variable
+    int result;
+    result = number* 2;
+        return result; 
+
+    }
+
+
+}
